2c931836e965804beedbe3229dab33e4e1b9439e
PinkBeeMe <56292374+pink-bee-me@users.noreply.github.com>
2021-12-13 05:49:47 -0600

    Initial Upload of File Content for CreatingTheCode.com
    Upload CreatingTheCode.com Portfolio Website Assets (Hosted On GoDaddy)  To GitHub

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+
+  
+  // These functions open and close the contact form 
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  
+  }
+
+  function buttonPink() {
+    document.getElementById("myForm").style.display="block";
+  }
+
+  function whereAreYou() {
+  
+// Add active class to the current button (highlight it)
+var header = document.getElementById("Navbar");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+  }
